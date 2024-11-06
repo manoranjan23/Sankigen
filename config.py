63a7543c755dd2517343255954a1e72621858f0b
manoranjan23 <manoranjanhor43@gmail.com>
2024-11-07 00:48:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "27884171"))
 API_HASH = getenv("API_HASH", "abe760b5d6b33e15c676577d6ae4a06a")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "7635240922:AAFNmk85MLBBZvrAh-_dbSS6CBxPPP2Eueg")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Somu:Somu@somu.xbkiklu.mongodb.net/?retryWrites=true&w=majority&appName=Somu")
 
 OWNER_ID = int(getenv("OWNER_ID", 7070591202))
